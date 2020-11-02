dc4a32aa425d237ec9c8cee58a4a02fd0595c994
MajoGro <majo1233rg@gmail.com>
2020-11-02 23:59:26 +0100

    7. Adding an executive functions in a row in Migration file after deploying contract

--- ethereum-course-advanced/Helloworld/migrations/2_Helloworld_deploy.js
+++ ethereum-course-advanced/Helloworld/migrations/2_Helloworld_deploy.js
@@ -2,6 +2,10 @@ const HelloWorld = artifacts.require("HelloWorld");
 
 module.exports = function(deployer) {
   deployer.deploy(HelloWorld).then(function(instance){
-    instance.setMessage("Changing the contract message: Hi Majo");
+    instance.setMessage("Changing the contract message: Hi Majo").then(function(){
+      instance.getMessage().then(function(message){
+        console.log("Current message: "" + message);
+      });
+    });
   });
 };
