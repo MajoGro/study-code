2d6b0760a2521e13c432b66c67aceb84f833605f
MajoGro <majo1233rg@gmail.com>
2020-11-02 23:38:38 +0100

    5. Changin the message of contract in Migration file after deploying contract

--- ethereum-course-advanced/Helloworld/migrations/2_Helloworld_deploy.js
+++ ethereum-course-advanced/Helloworld/migrations/2_Helloworld_deploy.js
@@ -1,5 +1,7 @@
 const HelloWorld = artifacts.require("HelloWorld");
 
 module.exports = function(deployer) {
-  deployer.deploy(HelloWorld);
+  deployer.deploy(HelloWorld).then(function(instance){
+    instance.setMessage("Changing the contract message: Hi Majo");
+  });
 };
