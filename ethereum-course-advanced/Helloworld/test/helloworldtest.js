d0ffdd31743bcb42fdebde04919a1613f233b195
MajoGro <majo1233rg@gmail.com>
2020-11-06 23:09:35 +0100

    13. Unit testing 2

--- ethereum-course-advanced/Helloworld/test/helloworldtest.js
+++ ethereum-course-advanced/Helloworld/test/helloworldtest.js
@@ -4,6 +4,14 @@ contract("HelloWorld", async function(){
   it("should initialize correctly", async function(){
     let instance = await HelloWorld.deployed();
     let message = await instance.getMessage();
-    assert( message === "Changing the contract message: Hi Majo");  
+    assert( message === "Changing the contract message: Hi Majo"
+    , "Message should be Changing the contract message: Hi Majo" );
+  });
+  it("should set message correctly", async function(){
+    let instance = await HelloWorld.deployed();
+    await instance.setMessage("Testing message");
+    let message = await instance.getMessage();
+    assert( message === "Testing message"
+    , "Message should be the same as we set it to" );
   });
 });
