2c69d095df149024512a11e44940c9ecfeb29959
MajoGro <majo1233rg@gmail.com>
2020-11-24 23:48:12 +0100

    28. Connecting Peopleproject account

--- ethereum-course-advanced/PeopleDapp/main.js
+++ ethereum-course-advanced/PeopleDapp/main.js
@@ -3,7 +3,8 @@ var contractInstance;
 
 $(document).ready(function() {
     window.ethereum.enable().then(function(accounts){
-      contractInstance = new web.eth.Contract(abi, 0xE42bAFbeb7f695686C883E3DBCC050c48E3718AC)
+      contractInstance = new web.eth.Contract(abi, "0xE42bAFbeb7f695686C883E3DBCC050c48E3718AC", {from: accounts[0]});
+      console.log(contractInstance);
 
     });
 });
