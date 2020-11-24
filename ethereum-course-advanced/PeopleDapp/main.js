8ed4dd9eac0883550760064996d0d30bfa2f630d
MajoGro <majo1233rg@gmail.com>
2020-11-24 23:08:29 +0100

    27. Adding abi.js file in People Dapp

--- ethereum-course-advanced/PeopleDapp/main.js
+++ ethereum-course-advanced/PeopleDapp/main.js
@@ -1,5 +1,9 @@
 var web3 = new Web3(Web3.givenProvider);
+var contractInstance;
 
 $(document).ready(function() {
-    window.ethereum.enable();
+    window.ethereum.enable().then(function(accounts){
+      contractInstance = new web.eth.Contract(abi, 0xE42bAFbeb7f695686C883E3DBCC050c48E3718AC)
+
+    });
 });
